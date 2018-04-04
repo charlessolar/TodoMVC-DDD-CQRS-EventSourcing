98c6ecbd18f8f1beaeb6ce1a4e75ad85251baef9
Charles Solar <charlessolar@gmail.com>
2018-04-04 16:02:48 -0500

    support eventstore host being a domain

--- server/Domain/Endpoint.cs
+++ server/Domain/Endpoint.cs
@@ -128,7 +128,7 @@ namespace Example
                 throw new ArgumentException($"Could not parse eventstore IP {host}");
 
 
-            var endpoints = new[] { new IPEndPoint(endpoint, 2113) };
+            var endpoints = new[] { GetIPEndPointFromHostName(host, 2113) };
             var cred = new UserCredentials(user, password);
             var settings = EventStore.ClientAPI.ConnectionSettings.Create()
                 .KeepReconnecting()
@@ -142,6 +142,26 @@ namespace Example
                 .SetDefaultUserCredentials(cred);
 
             return EventStoreConnection.Create(settings, endpoints.First(), "Domain");
+        }
+        // https://stackoverflow.com/a/2101787/223547
+        public static IPEndPoint GetIPEndPointFromHostName(string hostName, int port, bool throwIfMoreThanOneIP = false)
+        {
+            var addresses = System.Net.Dns.GetHostAddresses(hostName);
+            if (addresses.Length == 0)
+            {
+                throw new ArgumentException(
+                    "Unable to retrieve address from specified host name.",
+                    "hostName"
+                );
+            }
+            else if (throwIfMoreThanOneIP && addresses.Length > 1)
+            {
+                throw new ArgumentException(
+                    "There is more that one IP address to the specified host.",
+                    "hostName"
+                );
+            }
+            return new IPEndPoint(addresses[0], port); // Port gets validated here.
         }
     }
 }
